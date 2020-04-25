81f31a09729e476ae382e131f46d9a44dc1f2d1c
Amy Gori <amydgori@gmail.com>
2020-04-25 16:03:35 -0400

    Add intro js exercises

---
+++ js-hello-world/main.js
@@ -0,0 +1,35 @@
+// Intro JS exercise
+console.log('oh hai! Welcome to the console!')
+console.log('You can run JavaScript directly here.')
+console.log('You can also log any output from your JavaScript file.')
+
+// 1. What does `firstName` evaluate to on this line? Console.log to find out.
+let firstName
+
+// 2. What would you say what is happening here?
+// what does `firstName` evaluate to now?
+firstName = "Avery"
+
+// 3. What is happening here? Use console.log to check if you're right.
+let name = firstName
+
+// 4. Assign a value to the `greeting` variable that is a sentence that greets a user.
+// Then, use the value stored in the `firstName` variable and combine it with the greeting so it says something like .
+let greeting
+console.log(greeting)
+
+// 5. Combine the `greeting` and `name` variables to output a personalized greeting. Log it to the console.
+// Your code here
+
+// 6. What does `result` evaluate to?
+let num1 = 50
+let num2  = 10
+let result = num1 - num2
+
+// 7. What will `result` return here? Log it to the console to find out.
+result = num1 - "5"
+
+// 8. Create a variable to calculate the total number of tacos and burritos.
+// Console.log a sentence that tells the user the total number.
+let tacos = 8
+let burritos = 4
