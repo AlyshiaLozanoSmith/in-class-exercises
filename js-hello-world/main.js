27066f5df535c232cd43f994163396fd1d60a173
Amy Gori <amydgori@gmail.com>
2020-04-26 22:18:06 -0400

    Update README and comment out exercises to be uncommented when run

--- js-hello-world/main.js
+++ js-hello-world/main.js
@@ -8,28 +8,95 @@ let firstName
 
 // 2. What would you say what is happening here?
 // what does `firstName` evaluate to now?
-firstName = "Avery"
+// Uncomment the following lines and see what it is with console.log:
+// firstName = "Avery"
+// console.log("The firstName variable now has an assigned value of ", firstName)
 
-// 3. What is happening here? Use console.log to check if you're right.
-let name = firstName
+// 3. What is happening here? Use console.log() to check if you're right.
+// Uncomment the following line:
+// let name = firstName
 
 // 4. Assign a value to the `greeting` variable that is a sentence that greets a user.
-// Then, use the value stored in the `firstName` variable and combine it with the greeting so it says something like .
+// Then, use the value stored in the `firstName` variable above
+// and combine it with the greeting so it says something like "Hello, Avery!"
 let greeting
-console.log(greeting)
+
 
 // 5. Combine the `greeting` and `name` variables to output a personalized greeting. Log it to the console.
 // Your code here
 
 // 6. What does `result` evaluate to?
-let num1 = 50
-let num2  = 10
-let result = num1 - num2
+// Uncomment the following lines and use console.log() to check what value is store in the variable `result`
+// let num1 = 50
+// let num2  = 10
+// let result = num1 - num2
 
 // 7. What will `result` return here? Log it to the console to find out.
-result = num1 - "5"
+// Uncomment the following line:
+// result = num1 - "5"
 
 // 8. Create a variable to calculate the total number of tacos and burritos.
-// Console.log a sentence that tells the user the total number.
-let tacos = 8
-let burritos = 4
+// Console.log() a sentence that tells the user the total number.
+// Uncomment the following lines:
+// let tacos = 8
+// let burritos = 4
+
+
+/*
+Conditionals: using if/else statements
+Uncomment the necessary lines one at a time as you run the code.
+*/
+
+// 1. What will be logged to the console when the statement below runs?
+// let x;
+
+// if (x) {
+//   console.log("True!");
+// } else {
+//   console.log("False!");
+// }
+
+
+// 2. Which branch of the if/else statement will run here?
+// if ("") {
+//   console.log("True!");
+// } else {
+//   console.log("False!");
+// }
+
+
+// 3. Can you predict what will happen when you run the following?
+// var x = 1 * undefined;
+
+// if (x) {
+//   console.log("True!");
+// } else {
+//   console.log("False!");
+// }
+
+
+// 4.
+// if (-5 && 5) {
+//   console.log("True!");
+// } else {
+//   console.log("False!");
+// }
+
+
+// 5.
+// if ("Hello World!") {
+//   console.log("True!");
+// } else {
+//   console.log("False!");
+// }
+
+
+// 6. Try running this a few times, changing the boolean values for a and b so that the logic will run with different combinations. Observe the outcomes.
+// var a = false;
+// var b = true;
+
+// if (a || b) {
+//   console.log("True!");
+// } else {
+//   console.log("False!");
+// }
